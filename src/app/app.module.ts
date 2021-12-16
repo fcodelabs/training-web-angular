e944f3636149300fca68ca2f60b4b0520ed8ae9b
Dushan Pramod <dushanpramod@gmail.com>
2021-12-16 13:31:04 +0530

    change the module name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DiaryCardComponent } from './module-1/components/diary-card/diary-card.component';
+import { DiaryCardComponent } from './diarycardmodule/components/diary-card/diary-card.component';
 import { LayoutModule } from '@progress/kendo-angular-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GridModule } from '@progress/kendo-angular-grid';
@@ -11,16 +11,16 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {FormFieldModule, TextAreaModule} from "@progress/kendo-angular-inputs";
 import {LabelModule} from "@progress/kendo-angular-label";
 import { StoreModule } from '@ngrx/store';
-import {diarycardsReducer} from "./module-1/store/cards.reducer";
+import {diarycardsReducer} from "./diarycardmodule/store/cards.reducer";
 
 import {AngularFireModule} from "@angular/fire/compat";
 import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
 import {environment} from "../environments/environment";
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import {DiaryCardEffect} from "./module-1/store/cards.effects";
-import { FormComponent } from './module-1/components/form/form.component';
-import { HomeComponent } from './module-1/containers/home/home.component';
+import {DiaryCardEffect} from "./diarycardmodule/store/cards.effects";
+import { FormComponent } from './diarycardmodule/components/form/form.component';
+import { HomeComponent } from './diarycardmodule/containers/home/home.component';
 
 @NgModule({
   declarations: [
