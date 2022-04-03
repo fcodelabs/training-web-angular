13b97bed42f2ce2bf267bcd29fa8dff1becf6915
KrishalikaR <krishalika.r@synergenhealth.com>
2022-04-04 01:42:44 +0530

    firebase setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCNuT3hzOQ2DGQdZjn5NgQRCQttTeqUoqE",
+    authDomain: "dear-diary-krishalika.firebaseapp.com",
+    projectId: "dear-diary-krishalika",
+    storageBucket: "dear-diary-krishalika.appspot.com",
+    messagingSenderId: "367808969036",
+    appId: "1:367808969036:web:2bcb8851c695a08565b7e9",
+    measurementId: "G-1YQL7WSW5B"
+  }
 };
 
 /*
