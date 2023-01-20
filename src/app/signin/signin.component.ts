933d4cf981875822dbef237a28a7f2de0b35649f
Thilina Sandakelum <thilinas.19@cse.mrt.ac.lk>
2023-01-20 18:53:39 +0530

    signin page created

---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { uniqueNamesGenerator, Config, names } from 'unique-names-generator';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class SigninComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  public registerForm: FormGroup = new FormGroup({
+    userName: new FormControl(),
+  });
+
+  public submitForm(): void {
+    this.registerForm.markAllAsTouched();
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    }
+  }
+
+  public randomNameGeneratetor(): void {
+    const config: Config = {
+      dictionaries: [names],
+      separator: ' ',
+      style: 'capital',
+    };
+    const randomName = uniqueNamesGenerator(config);
+    this.registerForm.controls['userName'].setValue(randomName);
+  }
+}
