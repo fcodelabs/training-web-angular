b180e4fd1e551ae4b875c48bdcde9babd636e9b5
Nuwa-Hub <you@example.com>
2023-01-20 19:18:16 +0530

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
+import { SignInComponent } from './containers/sign-in/sign-in.component';
+import { HomePageComponent } from './containers/home-page/home-page.component';
+import { DataCardComponent } from './components/data-card/data-card.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,  SignInComponent, HomePageComponent, DataCardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
-    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
