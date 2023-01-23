780a9378877ba7928b137da768ac8108206f873c
Nuwa-Hub <you@example.com>
2023-01-23 09:16:35 +0530

    add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'dailydiary-96e2f',
+    appId: '1:472799620573:web:9379d3e893b530c1ebda34',
+    databaseURL: 'https://dailydiary-96e2f-default-rtdb.firebaseio.com',
+    storageBucket: 'dailydiary-96e2f.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCAHFAKZzgiuO_dQsvf7JsU7ABr7oFNdxk',
+    authDomain: 'dailydiary-96e2f.firebaseapp.com',
+    messagingSenderId: '472799620573',
+    measurementId: 'G-S85Q3ZNDT9',
+  },
   production: false
 };
 
