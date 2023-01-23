a627ed45c4d20ab09dd2a115325a321f35814665
rashidya <rashmiliyanage2000@gmail.com>
2023-01-24 00:45:36 +0530

    Firebase Firestore setup done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+    firebase:{
+        "apiKey":"AIzaSyDwVuFC-b0HoiBq_MS4f3ZEzNonhiyi-Rs",
+        "authDomain":"dear-diary-rashmi.firebaseapp.com",
+        "projectId":"dear-diary-rashmi",
+        "storageBucket":"dear-diary-rashmi.appspot.com",
+        "massegingSenderId":"548854194652",
+        "appId":"1:548854194652:web:cbde1e4a51dc72258a2f19",
+        "measurementId":"G-WTJZT1L6B9",
+        "databaseURL":"https://dear-diary-rashmi-default-rtdb.firebaseio.com/"
+    },
     production: false,
 }
 
