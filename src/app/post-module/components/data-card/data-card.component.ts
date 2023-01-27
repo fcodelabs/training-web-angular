1849d9c84ef4f128368afc420c4273e19f78472b
Nuwa-Hub <you@example.com>
2023-01-27 10:10:29 +0530

    change format of card list/and add auth gard and loading component

--- src/app/post-module/components/data-card/data-card.component.ts
+++ src/app/post-module/components/data-card/data-card.component.ts
@@ -12,6 +12,5 @@ export class DataCardComponent {
    isExpanded = false;
   public thumbnailSrc =
     'https://firebasestorage.googleapis.com/v0/b/dailydiary-96e2f.appspot.com/o/avatar-user-computer-icons-software-developer-png-favpng-7SbFpNeqKqhhTrrrnHFUqk6U4.jpg?alt=media&token=78f7c5fc-99fc-4616-b5ca-77be651a93fd';
-  public cover =
-    'https://www.telerik.com/kendo-angular-ui-develop/components/layout/card/assets/black_sea.jpg';
+ 
 }
--- src/app/sign-in-module/containers/sign-in/sign-in.component.ts
+++ src/app/sign-in-module/containers/sign-in/sign-in.component.ts
@@ -1,26 +1,26 @@
-import { Component, ViewChild } from '@angular/core';
-import * as chance from 'chance';
-import { ViewEncapsulation } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import * as UserActions from '../../../store/actions/userAction';
-import { Store, select } from '@ngrx/store';
-import { AppStateInterface } from 'src/app/types/appState.interface';
-import { Router } from '@angular/router';
+import { Component, ViewChild } from "@angular/core";
+import * as chance from "chance";
+import { ViewEncapsulation } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import * as UserActions from "../../../store/actions/userAction";
+import { Store, select } from "@ngrx/store";
+import { AppStateInterface } from "src/app/types/appState.interface";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss'],
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.scss"],
   encapsulation: ViewEncapsulation.None,
 })
 export class SignInComponent {
-   username: string;
+  username: string;
   focus = false;
   public thumbnailSrc =
-    'https://firebasestorage.googleapis.com/v0/b/dailydiary-96e2f.appspot.com/o/rose-flower-icon-vector-3720361.jpg?alt=media&token=6633787a-d348-4287-a29a-0e251002f1e0';
- 
+    "https://firebasestorage.googleapis.com/v0/b/dailydiary-96e2f.appspot.com/o/rose-flower-icon-vector-3720361.jpg?alt=media&token=6633787a-d348-4287-a29a-0e251002f1e0";
+
   constructor(private store: Store<AppStateInterface>, private router: Router) {
-    this.username = '';
+    this.username = "";
   }
 
   handleClick() {
@@ -31,20 +31,16 @@ export class SignInComponent {
     this.focus = value;
   }
   handleSubmit() {
-    if (this.username !== '') {
-      localStorage.setItem('username', this.username);
+    if (this.username !== "") {
+      localStorage.setItem("username", this.username);
       this.store.dispatch(
         UserActions.getUserSuccess({ user: { username: this.username } })
       );
-      this.router.navigate(['/home']);
+      this.router.navigate(["/home"]);
     } else {
-      alert('Please enter a username');
+      alert("Please enter a username");
     }
   }
-
- getusername() {
-console.log(this.username);
- }
 
   ngOnInit(): void {}
 }
