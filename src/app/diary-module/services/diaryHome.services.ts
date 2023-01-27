3f0ade62c686889d09155701f68ba5863a070d26
rashidya <rashmiliyanage2000@gmail.com>
2023-01-27 16:26:21 +0530

    Indentation fixed and unused imports removed

--- src/app/diary-module/services/diaryHome.services.ts
+++ src/app/diary-module/services/diaryHome.services.ts
@@ -3,19 +3,16 @@ import {
     addDoc,
     collection,
     onSnapshot,
-    query,
-    orderBy,
     CollectionReference,
     DocumentData,
     Firestore,
 } from '@angular/fire/firestore'
-import { Observable, of } from 'rxjs'
-import { delay } from 'rxjs/operators'
-import { Card } from '../components/diary-card/card'
+import { Observable } from 'rxjs'
+import { Card } from 'src/app/models/card'
 
 @Injectable({ providedIn: 'root' })
 export class DiaryHomeService {
-    cards: any[] = []
+    cards: Card[] = []
     cardCollection: CollectionReference<DocumentData>
 
     constructor(firestore: Firestore) {
@@ -23,30 +20,28 @@ export class DiaryHomeService {
     }
 
     getCards(): Observable<Card[]> {
-  
         return new Observable((observer) => {
             onSnapshot(this.cardCollection, (snapshot) => {
-                const cards:any[] = snapshot.docs.map((doc) => ({
+                const cards: any[] = snapshot.docs.map((doc) => ({
                     id: doc.id,
                     ...doc.data(),
-                }));
-                console.log(cards);
-                
+                }))
+                console.log(cards)
+
                 observer.next(cards)
             })
         })
     }
 
     addCard(card: Card) {
-
         const date = new Date()
         return new Observable((observer) => {
             addDoc(this.cardCollection, {
-              ...card,
-              created: date,
+                ...card,
+                created: date,
             }).then((doc) => {
-              observer.next({ ...card, id: doc.id } as Card);
-            });
-          });
+                observer.next({ ...card, id: doc.id } as Card)
+            })
+        })
     }
 }
