2ab3607c0715cc08ae1c5dd1bb30a508befa9650
Nuwa-Hub <you@example.com>
2023-01-29 12:52:34 +0530

    add formGroup for sign in page

--- src/app/containers/sign-in/sign-in.component.ts
+++ src/app/containers/sign-in/sign-in.component.ts
@@ -1,29 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import * as chance from 'chance';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, ViewChild } from "@angular/core";
+import * as chance from "chance";
+import { ViewEncapsulation } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import * as UserActions from "../../../store/actions/userAction";
+import { Store, select } from "@ngrx/store";
+import { AppStateInterface } from "src/app/types/appState.interface";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss'],
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
-export class SignInComponent implements OnInit {
-   form: FormGroup;
-  username = new FormControl('');
+export class SignInComponent {
+  public thumbnailSrc =
+    "https://firebasestorage.googleapis.com/v0/b/dailydiary-96e2f.appspot.com/o/rose-flower-icon-vector-3720361.jpg?alt=media&token=6633787a-d348-4287-a29a-0e251002f1e0";
 
-  constructor() {
-    this.form = new FormGroup({
-      name: this.username
-    });
-  }
-
+  constructor(
+    private router: Router
+  ) {}
   ngOnInit(): void {}
+
+  public loginForm: FormGroup = new FormGroup({
+    username: new FormControl(),
+  });
 
   handleClick() {
     const generator = new chance();
-    this.username = generator.name();
+    this.loginForm.controls["username"].setValue(generator.name());
   }
- onSubmit() {
-    console.log(this.form.value);
+
+  submitForm(): void {
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.valid) {
+      localStorage.setItem("username", this.loginForm.value.username);
+      this.router.navigate(["/home"]);
+    } else {
+      alert("Please enter a username");
+    }
   }
 }
