1a0d030a8cb34624649f3a87210a7c9b8996a607
Thilina Sandakelum <thilinas.19@cse.mrt.ac.lk>
2023-01-31 11:08:22 +0530

    make requested changes to Input decorator

--- src/app/components/card/card.components.ts
+++ src/app/components/card/card.components.ts
@@ -1,30 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+interface InputData {
+  id: string;
+  name: string;
+  description: string;
+  title: string;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  input = {
-    id: '',
-    name: '',
-    description: '',
-    title: '',
-  };
+  @Input() data: InputData;
 
   seeMore: boolean = false;
 
   showAction: boolean = false;
 
-  constructor() {}
+  constructor() {
+    this.data = {
+      id: '1',
+      name: 'Name',
+      description: 'Description',
+      title: 'Title',
+    } as InputData;
+  }
 
   ngOnInit(): void {
     //dummy data
-    this.input.description =
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-    this.input.name = 'Card Name';
-    this.input.title = 'Card Title';
   }
 
   public buttonClick(): void {
