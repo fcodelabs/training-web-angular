d9590919b0c06e33b506ecc752ca66f226005020
DLP-Sekara <dslahirupiyumal99@gmail.com>
2023-02-03 09:26:09 +0530

    Create diary Card

--- dear-diary-angular/src/app/app.module.ts
+++ dear-diary-angular/src/app/app.module.ts
@@ -5,13 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInPageComponent } from './containers/sign-in-page/sign-in-page.component';
 import { HomePageComponent } from './containers/home-page/home-page.component';
+import { DiaryCardComponent } from './components/diary-card/diary-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent, 
     SignInPageComponent, 
-    HomePageComponent],
+    HomePageComponent, DiaryCardComponent],
   imports: [
     BrowserModule, 
     AppRoutingModule, 
