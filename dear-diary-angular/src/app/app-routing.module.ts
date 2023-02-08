8a8b0598824213ac8bc2b5a9bda25a570ab62161
DLP-Sekara <dslahirupiyumal99@gmail.com>
2023-02-08 21:23:16 +0530

    Create sign In page

--- dear-diary-angular/src/app/app-routing.module.ts
+++ dear-diary-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInPageComponent } from './containers/sign-in-page/sign-in-page.component';
+import { HomePageComponent } from './containers/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SignInPageComponent },
+  { path: 'home', component: HomePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- dear-diary-angular/src/app/app.module.ts
+++ dear-diary-angular/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInPageComponent } from './containers/sign-in-page/sign-in-page.component';
+import { HomePageComponent } from './containers/home-page/home-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SignInPageComponent, HomePageComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
